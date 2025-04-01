4bf0114ac59752512b93d183331a8d7ff92e18e4
natuliochek <natvicor@mail.ru>
2025-04-01 16:55:12 +0200

    first commit

---
+++ src/data.jsx
@@ -0,0 +1,37 @@
+export const data = [
+    {
+        id: 1,
+        name: "2018 july",
+        image: "https://images.unsplash.com/photo-1741851373559-6879db14fd8a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8RnpvM3p1T0hONnd8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        id: 2,
+        name: "2023 october",
+        image: "https://images.unsplash.com/photo-1742943679519-ee406c510232?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDN8RnpvM3p1T0hONnd8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        id: 3,
+        name: "2022 september",
+        image: "https://images.unsplash.com/photo-1742855751015-5bda25456249?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDR8RnpvM3p1T0hONnd8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        id: 4,
+        name: "2025 january",
+        image: "https://images.unsplash.com/photo-1739382445476-61ff0ae36125?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDZ8RnpvM3p1T0hONnd8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        id: 5,
+        name: "2019 january",
+        image: "https://images.unsplash.com/photo-1742218409795-ed231ecbd9ad?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDEzfEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fHw%3D"
+    },
+    {
+        id: 6,
+        name: "2021 march",
+        image: "https://images.unsplash.com/photo-1742201929743-90e14da35f28?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIwfEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fHw%3D"
+    },
+    {
+        id: 7,
+        name: "2024 april",
+        image: "https://images.unsplash.com/photo-1742590794643-5b401ed198b4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIxfEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fHw%3D"
+    }
+]
